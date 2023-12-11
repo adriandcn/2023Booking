24eb273691b411f9f3387ebe260677f47eaa2411
Adrian Cevallos <ADRIANCE@iadb.org>
2023-12-10 21:11:50 -0500

    stop4

--- app/controllers/pjFront.controller.php
+++ app/controllers/pjFront.controller.php
@@ -719,7 +719,7 @@ class pjFront extends pjAppController
 
 		//if ($this->isXHR())
 		//{
-
+			echo 'Stop4 ';
 			$fechaInicio = date("Y-m-d", $_SESSION[$this->defaultCalendar]['start_dt']);
 			$conn = new mysqli(PJ_DB_HOST, PJ_DB_USERNAME, PJ_DB_PASS, PJ_DB_NAME);
 			if($conn->connect_errno){
