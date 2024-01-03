a06fb85a0f83e6a5972e321abf7e7575aea2ac05
Adrian Cevallos <ADRIANCE@iadb.org>
2024-01-03 17:16:25 -0500

    all

--- app/web/js/pjABCalendar.js
+++ app/web/js/pjABCalendar.js
@@ -238,6 +238,7 @@ console.log('Precio seguro',num1);
 				// console.log('SASDSSADASD: ', cuponID) ;
 				//console.log('Data Stop19: ', opts);
 				var dataQ=sessionStorage.getItem('dataQ');
+				console.log("DataQ: ", dataQ);
 				pjQ.$.post([self.opts.folder, "index.php?controller=pjFront&action=pjActionBookingSave&cid=", self.opts.cid, "&check=", checkUpdatePrice, "&cupon=", valorCupon, "&dataQ=", dataQ, "&cupon_id=",cuponID, "&session_id=", self.opts.session_id].join("")).done(function (data) {
 				//pjQ.$.post([self.opts.folder, "index.php?controller=pjFront&action=pjActionBookingSave&cid=", self.opts.cid].join("")).done(function (data) {
 					//data.payment_method='creditcard';
